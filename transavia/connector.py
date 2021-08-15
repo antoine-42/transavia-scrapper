911ef126aaf026a271d330f0367a558c720b2ba8
Antoine Dujardin <antoinedujardin42@gmail.com>
2021-08-15 12:58:54 +0200

    parse flight buttons

--- transavia/connector.py
+++ transavia/connector.py
@@ -1,15 +1,19 @@
 from datetime import date, datetime
-from typing import Optional, List
+from typing import List
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class Travel:
-    def __init__(self, origin: str, destination: str, departure_date: date, return_date: date):
+    def __init__(
+        self, origin: str, destination: str, departure_date: date, return_date: date
+    ):
         self.origin = origin
         self.destination = destination
         self.departure_date = departure_date
@@ -17,15 +21,34 @@ class Travel:
 
 
 class Flight:
-    def __init__(self, origin: str, destination: str, departure_time: datetime, arrival_time: datetime):
+    def __init__(
+        self,
+        number: str,
+        origin: str,
+        destination: str,
+        departure_time: datetime,
+        arrival_time: datetime,
+        price: int,
+    ):
+        self.number = number
         self.origin = origin
         self.destination = destination
         self.departure_time = departure_time
         self.arrival_time = arrival_time
+        self.price = price
+
+    def __str__(self) -> str:
+        return (
+            f"{self.number} | {self.origin} | {self.destination}"
+            + f" | {self.departure_time.time().isoformat(timespec='minutes')}"
+            + f" | {self.departure_time.time().isoformat(timespec='minutes')}"
+            + f" | {self.price}€"
+        )
 
 
 class TransaviaConnector:
     """Handles contact with the Transavia website through Selenium"""
+
     URL_HOME = "https://www.transavia.com/fr-FR/accueil/"
     URL_SEARCH = "https://www.transavia.com/fr-FR/reservez-un-vol/vols/rechercher/"
 
@@ -40,13 +63,41 @@ class TransaviaConnector:
         # self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver = webdriver.Firefox()
 
-    def get_flights(self, base) -> List[Flight]:
-        outbound_flights_selectors = base.find_element_by_css_selector(".nav-days .animation-container form")
-        for flight_date_selector in outbound_flights_selectors.find_elements_by_css_selector(".day-with-availability"):
+    @staticmethod
+    def str_to_datetime(value: str) -> datetime:
+        return datetime.strptime(value, "%d/%m/%Y %H:%M")
+
+    @staticmethod
+    def deserialize_flight_button(button: WebElement) -> Flight:
+        value = button.get_attribute("value")
+        parts = [v for v in value.split("|")[1].split("~") if v and v not in [" "]]
+        return Flight(
+            number="".join(parts[0:2]),
+            origin=parts[2],
+            destination=parts[4],
+            departure_time=TransaviaConnector.str_to_datetime(parts[3]),
+            arrival_time=TransaviaConnector.str_to_datetime(parts[5]),
+            price=button.find_element_by_css_selector("div.price").text,
+        )
+
+    def get_flights(self, base: WebElement) -> List[Flight]:
+        flights = []
+        outbound_flights_selectors = base.find_element_by_css_selector(
+            ".nav-days .animation-container form"
+        )
+        for (
+            flight_date_selector
+        ) in outbound_flights_selectors.find_elements_by_css_selector(
+            ".day-with-availability"
+        ):
             flight_date_selector.find_element_by_css_selector(".button").click()
-            for flight_button in base.find_elements_by_css_selector("button.flight-result-button"):
-                flight_data = flight_button.get_attribute("value")
-                print(flight_data)
+            for flight_button in base.find_elements_by_css_selector(
+                "button.flight-result-button"
+            ):
+                flights.append(
+                    TransaviaConnector.deserialize_flight_button(flight_button)
+                )
+        return flights
 
     def search_travel(self, travel: Travel) -> List[Flight]:
         """Find travels matching the provided travel. Ignores time, only the date is used.
@@ -56,12 +107,31 @@ class TransaviaConnector:
         """
         self.driver.get(TransaviaConnector.URL_HOME)
 
-        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.ID, "routeSelection_DepartureStation-input")))
+        WebDriverWait(self.driver, 60).until(
+            EC.presence_of_element_located(
+                (By.ID, "routeSelection_DepartureStation-input")
+            )
+        )
 
-        origin_input = self.driver.find_element_by_css_selector("input#routeSelection_DepartureStation-input")
-        destination_input = self.driver.find_element_by_css_selector("input#routeSelection_ArrivalStation-input")
-        departure_input = self.driver.find_element_by_css_selector("input#dateSelection_OutboundDate-datepicker")
-        return_input = self.driver.find_element_by_css_selector("input#dateSelection_IsReturnFlight-datepicker")
+        try:
+            self.driver.find_element_by_css_selector(
+                "button.info-banner-button-all.button-call-to-action"
+            ).click()
+        except NoSuchElementException:
+            pass  # No cookie banner
+
+        origin_input = self.driver.find_element_by_css_selector(
+            "input#routeSelection_DepartureStation-input"
+        )
+        destination_input = self.driver.find_element_by_css_selector(
+            "input#routeSelection_ArrivalStation-input"
+        )
+        departure_input = self.driver.find_element_by_css_selector(
+            "input#dateSelection_OutboundDate-datepicker"
+        )
+        return_input = self.driver.find_element_by_css_selector(
+            "input#dateSelection_IsReturnFlight-datepicker"
+        )
 
         origin_input.send_keys(travel.origin)
         origin_input.send_keys(Keys.RETURN)
@@ -69,19 +139,27 @@ class TransaviaConnector:
         destination_input.send_keys(Keys.RETURN)
         departure_input.clear()
         departure_input.send_keys(travel.departure_date.strftime("%d-%m-%Y"))
-        departure_input.send_keys(Keys.RETURN)
+        origin_input.click()
         return_input.clear()
         return_input.send_keys(travel.return_date.strftime("%d-%m-%Y"))
-        return_input.send_keys(Keys.RETURN)
+        origin_input.click()
         destination_input.send_keys(Keys.RETURN)
 
-        outbound_box = self.driver.find_element_by_css_selector("section.flight.outbound")
+        # Solve Captchas
+
+        outbound_box = self.driver.find_element_by_css_selector(
+            "section.flight.outbound"
+        )
         flights = self.get_flights(outbound_box)
+
+        return_box = self.driver.find_element_by_css_selector("section.flight.inbound")
+        flights += self.get_flights(return_box)
 
         return flights
 
 
 if __name__ == "__main__":
-    t = Travel("Paris", "Amsterdam", date(2021, 10, 22), date(2021, 10, 22))
+    t = Travel("Paris", "Amsterdam", date(2021, 10, 21), date(2021, 11, 22))
     connector = TransaviaConnector(False)
-    connector.search_travel(t)
+    flights = connector.search_travel(t)
+    print(flights)
